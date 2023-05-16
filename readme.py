d659d16f35cf5a097c6ca36394d656cc02ea7857
William <wdnconsultancy@yahoo.com>
2023-05-16 16:41:29 +0800

    how to use check50 and submit50

---
+++ readme.py
@@ -0,0 +1,3 @@
+# refer to read me doucment
+# but essenitally for cs50 in order to prompt for github username and password whereu will type the token key instead u need to log out from signed accoutn in vscode, every step of check50 will prompt for password again where u key in the token again
+# of course u have to make sure prior to this you have pip installed check50 and submit 50 refer to cs50 read the docs for documentation https://cs50.readthedocs.io/github/
